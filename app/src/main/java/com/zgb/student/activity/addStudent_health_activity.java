e713e6874c959bc5f298bbfce401fc61d41b7f00
zgb <454923159@qq.com>
2018-04-01 19:43:14 +0800

    20180331

--- app/src/main/java/com/zgb/student/activity/addStudent_health_activity.java
+++ app/src/main/java/com/zgb/student/activity/addStudent_health_activity.java
@@ -5,19 +5,32 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
-
+import android.widget.ListView;
 import com.zgb.student.R;
+import com.zgb.student.model.Student;
 import com.zgb.student.tools.DatabaseHelper;
+import com.zgb.student.tools.QueryAdapter;
+import com.zgb.student.tools.StudentAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 /**
- * 添加学生信息的界面,修改学生信息的界面
- * Created by com.zgb on 2016/10/1.
+ * 增加学生健康信息
  */
 public class addStudent_health_activity extends Activity {
 
@@ -27,7 +40,10 @@ public class addStudent_health_activity extends Activity {
     private EditText measureDate;
     private EditText password;
     private EditText info;
+    private EditText mEditText;
+    private ListView mListView;
 
+    Cursor cursor;
 
     private String oldID;//用于防治修改信息时将ID也修改了，而原始的有该ID的学生信息还保存在数据库中
 
@@ -42,6 +58,9 @@ public class addStudent_health_activity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.add_student_health);
 
+        dbHelper = DatabaseHelper.getInstance(this);
+
+    //    initView();
         name = (EditText) findViewById(R.id.add_student_health_name);
         sex = (EditText) findViewById(R.id.add_student_health_sex);
         id = (EditText) findViewById(R.id.add_student_health_id);
@@ -51,7 +70,6 @@ public class addStudent_health_activity extends Activity {
     //    chinese = (EditText) findViewById(R.id.add_student_layout_chinese);
     //    english = (EditText) findViewById(R.id.add_student_layout_english);
 
-        dbHelper = DatabaseHelper.getInstance(this);
 
         oldData = getIntent();
         if (oldData.getStringExtra("haveData").equals("true")) {
@@ -128,5 +146,77 @@ public class addStudent_health_activity extends Activity {
 //        english.setText(String.valueOf(englishScore));
     }
 
+    private void initView() {
 
+//        mEditText = (EditText) findViewById(R.id.edittext);
+//        mListView = (ListView) findViewById(R.id.listview);
+//
+//        findViewById(R.id.listview);
+
+
+        //EditText添加监听
+        mEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}//文本改变之前执行
+
+            @Override
+            //文本改变的时候执行
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //显示ListView
+                showListView();
+            }
+            @Override
+            public void afterTextChanged(Editable s) { }//文本改变之后执行
+        });
+
+
+    }
+
+    private void showListView() {
+        mListView.setVisibility(View.VISIBLE);
+        //获得输入的内容
+        String name = mEditText.getText().toString().trim();
+        //获取数据库对象
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+
+        List<Map<String,Object>> studentList =query(name);
+
+
+        SimpleAdapter adapter = new SimpleAdapter(addStudent_health_activity.this, studentList,R.layout.add_student_health, new String[] { "id",
+                "name" }, new int[] { R.id.add_student_health_id, R.id.add_student_health_name,
+                });
+
+        mListView.setAdapter(adapter);
+
+  //      cursor.close();
+    }
+
+    //初始化学生信息
+    private List<Map<String,Object>> query(String name) {
+        List<Map<String,Object>> studentList = new ArrayList<Map<String,Object>>();
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        String[] selectioinArgs = {"%"+name+"%"};
+        Cursor cursor = db.rawQuery("select * from student s where name like ? order by s.id", selectioinArgs);
+        Map<String, Object> map;
+        while (cursor.moveToNext()) {
+            String id = cursor.getString(cursor.getColumnIndex("id"));
+//            String name = cursor.getString(cursor.getColumnIndex("name"));
+//            String password = cursor.getString(cursor.getColumnIndex("password"));
+            String sex = cursor.getString(cursor.getColumnIndex("sex"));
+//            String info = cursor.getString(cursor.getColumnIndex("info"));
+//            String measureDate = cursor.getString(cursor.getColumnIndex("measureDate"));
+//            String number = cursor.getString(cursor.getColumnIndex("number"));
+//            int mathScore = cursor.getInt(cursor.getColumnIndex("mathScore"));
+//            int chineseScore = cursor.getInt(cursor.getColumnIndex("chineseScore"));
+//            int englishScore = cursor.getInt(cursor.getColumnIndex("englishScore"));
+//            int order=cursor.getInt(cursor.getColumnIndex("ranking"));
+            map=new HashMap<String, Object>();
+            map.put(id,name);
+            studentList.add(map);
+        }
+        cursor.close();
+        return studentList;
+
+    }
 }
